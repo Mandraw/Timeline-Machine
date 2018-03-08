62e8c03749ad3353066f7c8342e3932dd592765a
Oscar <M_Mandrake@hotmail.fr>
2018-03-08 16:51:58 +0100

    html update

--- modele/&mongoose.js
+++ modele/&mongoose.js
@@ -1,19 +1,16 @@
-var mongoose = require('mongoose')
-var options = options =  { poolSize : 10000 , replicaSet : "rs-ds261128", keepAlive: 1, connectTimeoutMS: 30000, reconnectTries: 30, reconnectInterval: 5000}
+// var mongoose = require('mongoose')
+// var options = options = { poolSize: 10000, replicaSet: 'rs-ds261128', keepAlive: 1, connectTimeoutMS: 30000, reconnectTries: 30, reconnectInterval: 5000}
 
-
-function dbSave(content) {
-    content.save(function (err) {
-        if (err) { 
-            console.log("Save Error")
-            throw err 
-        }
-        console.log('Database Updated !') 
-    })
+function dbSave (content) {
+  content.save(function (err) {
+    if (err) {
+      console.log('Save Error')
+      throw err
+    }
+    console.log('Database Updated !')
+  })
 }
 
-
-
 module.exports = {
-    dbSave
+  dbSave
 }
--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,14 @@ var express = require('express')
 var router = express.Router()
 
 /* GET home page. */
-router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Express' })
+router.get('/addTimeline', function (req, res, next) {
+  res.render('addTimeline')
 })
+  .get('/:page', function (req, res, next) {
+    res.render('index', { page: req.params.page })
+  })
+  .get('/', function (req, res, next) {
+    res.render('index')
+  })
 
 module.exports = router
